665e1354f268d34bd592d590b8cf209dc4319e76
Steven Braham <steven@braham.biz>
2017-06-26 12:49:30 +0200

    memory management

--- calculator.js
+++ calculator.js
@@ -1,12 +1,14 @@
 /**
  * Created by stevenbraham on 23-05-17.
  */
-
 //main logic engine that straps everything together
+
+'use strict';
 
 $(() => {
     //only init objects after jquery if fully available
     let displayEngine = new Display();
+    let memoryEngine = new Memory();
 
     //listen to click events
     $("button").click(function () { //$("button").click is short for $("button").on('click',...
@@ -17,6 +19,31 @@ $(() => {
             case "number":
                 //simple number, add to display and working memory
                 displayEngine.appendNumber(buttonContent);
+                break;
+            case "memory":
+                //memory storing and retrieving related functions
+                switch (buttonContent) {
+                    case "MR":
+                        //retrieve and display current memory number
+                        displayEngine.setNumber(memoryEngine.number);
+                        break;
+                    case "M+":
+                        //to improve efficiency I chain the output of several functions together
+                        displayEngine.setMemoryNumber(memoryEngine.addNumber(displayEngine.getNumber()));
+                        break;
+                    case "M-":
+                        displayEngine.setMemoryNumber(memoryEngine.subtractNumber(displayEngine.getNumber()));
+                        break;
+                    case "MC":
+                        //reset memory
+                        memoryEngine.number = 0;
+                        displayEngine.setMemoryNumber(memoryEngine.number);
+                        break;
+                    default:
+                        //Not supported/implemented
+                        throw `Memory function type ${buttonContent} is an unsupported operation`;
+                        break;
+                }
                 break;
             default:
                 //Not supported/implemented button type
